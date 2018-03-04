22904f70a3829f478b44af263c4f0fb1d1950b7d
buoyantair <buoyantair@gmail.com>
2018-03-04 18:10:18 +0530

    Add project files

---
+++ README.md
@@ -0,0 +1,4 @@
+# Trend tracker
+
+A simple trend tracker using twitter's streaming API.
+
---
+++ index.js
@@ -0,0 +1,26 @@
+const Twitter = require('twitter');
+const chalk = require('chalk');
+const dotenv = require('dotenv');
+dotenv.config();
+const log = console.log;
+
+const client = new Twitter({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+});
+
+const stream = client.stream('statuses/filter', {
+  track: 'react',
+});
+
+stream.on('data', function(event){
+  log(`${chalk.white.bgBlue(event.user.name)} tweets:`);
+  log(event.text);
+  log('****************************************');
+});
+
+stream.on('error', function(error) {
+  throw error;
+});
